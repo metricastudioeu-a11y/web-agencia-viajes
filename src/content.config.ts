106e9427a22c24c527362ab382fbd1fa09371894
metricastudioeu-a11y <metricastudio.eu@gmail.com>
2026-03-08 03:22:41 +0100

    Configurar colección de viajes y loaders
    Finalizando la configuración de Astro 5

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,12 @@
 import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders'; // Cambiamos ":" por "/" para máxima compatibilidad
+import { glob } from 'astro:loaders'; // IMPORTANTE: que use ":"
 
 const viajes = defineCollection({
-  // Busca tus archivos de viajes en esta carpeta específica
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/viajes" }),
   schema: ({ image }) => z.object({
     titulo: z.string(),
     descripcion: z.string(),
-    imagenPortada: image(),
+    imagenPortada: z.string(), 
     precio: z.number().optional(),
     destacado: z.boolean().default(false),
   }),
